0b654ac77262d1dd6dab57668d4ef5c48b04f66f
PROMETHIA <electriccobras@gmail.com>
2024-05-11 10:04:59 -0400

    add SerRef for convenience

---
+++ Unity/Assets/Collections/SerRef.cs
@@ -0,0 +1,17 @@
+using System;
+using UnityEngine;
+
+namespace RealityFlow.Collections
+{
+    [Serializable]
+    public struct SerRef<T>
+    {
+        [SerializeReference]
+        public T Value;
+
+        public SerRef(T value) => Value = value;
+
+        public static implicit operator T(SerRef<T> serRef) => serRef.Value;
+        public static implicit operator SerRef<T>(T value) => new(value);
+    }
+}
