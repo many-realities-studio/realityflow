ca87c55c6343c86e0891c0ac7ef556193fdf7055
PROMETHIA <electriccobras@gmail.com>
2024-05-13 13:39:54 -0400

    Add and test Actions/Funcs with arguments

--- Unity/Assets/Scripting/ScriptUtilities.cs
+++ Unity/Assets/Scripting/ScriptUtilities.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Reflection;
+using System.Text;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Scripting;
@@ -73,6 +74,27 @@ namespace RealityFlow.Scripting
             return Compile(csc, diagnostics);
         }
 
+        static string GetScriptCode(string body, Type returnType, (Type type, string name)[] arguments)
+        {
+            string argumentList = arguments
+                .Select(arg => $"{arg.type.FullName} {arg.name}")
+                .Aggregate((accum, next) => $"{accum}, {next}")
+                .ToString();
+            return $@"
+                #pragma warning disable CS8019
+                using System;
+                using System.Collections.Generic;
+                using UnityEngine;
+                using Ubiq;
+                
+                static class Script {{ 
+                    public static {returnType?.FullName ?? "void"} Eval({argumentList}) 
+                    {{ 
+                        {body};
+                    }} 
+                }}";
+        }
+
         /// <summary>
         /// Produces an Action from the body of a C# method. 
         /// If diagnostics is not null, adds any warnings/errors to it.
@@ -81,19 +103,7 @@ namespace RealityFlow.Scripting
         public static Action GetAction(string body, List<Diagnostic> diagnostics)
         {
             Assembly asm = CompileToAssembly(
-                $@"
-                #pragma warning disable CS8019
-                using System;
-                using System.Collections.Generic;
-                using UnityEngine;
-                using Ubiq;
-                
-                static class Script {{ 
-                    public static void Eval() 
-                    {{ 
-                        {body};
-                    }} 
-                }}",
+                GetScriptCode(body, returnType: null, Array.Empty<(Type, string)>()),
                 diagnostics
             );
             if (asm is null)
@@ -105,32 +115,119 @@ namespace RealityFlow.Scripting
         }
 
         /// <summary>
+        /// Produces an Action from the body of a C# method. 
+        /// If diagnostics is not null, adds any warnings/errors to it.
+        /// Returns null if not successful.
+        /// </summary>
+        public static Action<T1> GetAction<T1>(string body, List<Diagnostic> diagnostics, string arg1Name)
+        {
+            Assembly asm = CompileToAssembly(
+                GetScriptCode(body, returnType: null, new[] {
+                    (typeof(T1), arg1Name)
+                }),
+                diagnostics
+            );
+            if (asm is null)
+                return null;
+            return (Action<T1>)Delegate.CreateDelegate(
+                typeof(Action<T1>),
+                asm.GetType("Script").GetMethod("Eval")
+            );
+        }
+
+        /// <summary>
+        /// Produces an Action from the body of a C# method. 
+        /// If diagnostics is not null, adds any warnings/errors to it.
+        /// Returns null if not successful.
+        /// </summary>
+        public static Action<T1, T2> GetAction<T1, T2>(
+            string body,
+            List<Diagnostic> diagnostics,
+            string arg1Name,
+            string arg2Name
+        )
+        {
+            Assembly asm = CompileToAssembly(
+                GetScriptCode(body, returnType: null, new[] {
+                    (typeof(T1), arg1Name),
+                    (typeof(T2), arg2Name)
+                }),
+                diagnostics
+            );
+            if (asm is null)
+                return null;
+            return (Action<T1, T2>)Delegate.CreateDelegate(
+                typeof(Action<T1, T2>),
+                asm.GetType("Script").GetMethod("Eval")
+            );
+        }
+
+        /// <summary>
         /// Produces a Func<T> from the body of a C# method. 
         /// This C# method body must return a value of type T.
         /// If diagnostics is not null, adds any warnings/errors to it.
         /// Returns null if not successful.
         /// </summary>
-        public static Func<T> GetFunc<T>(string body, List<Diagnostic> diagnostics)
+        public static Func<R> GetFunc<R>(string body, List<Diagnostic> diagnostics)
         {
             Assembly asm = CompileToAssembly(
-                $@"
-                using System;
-                using System.Collections.Generic;
-                using UnityEngine;
-                using Ubiq;
-                
-                static class Script {{ 
-                    public static {typeof(T).FullName} Eval() 
-                    {{
-                        {body};
-                    }} 
-                }}",
+                GetScriptCode(body, typeof(R), Array.Empty<(Type, string)>()),
                 diagnostics
             );
             if (asm is null)
                 return null;
-            return (Func<T>)Delegate.CreateDelegate(
-                typeof(Func<T>),
+            return (Func<R>)Delegate.CreateDelegate(
+                typeof(Func<R>),
+                asm.GetType("Script").GetMethod("Eval")
+            );
+        }
+
+        /// <summary>
+        /// Produces a Func<T> from the body of a C# method. 
+        /// This C# method body must return a value of type T.
+        /// If diagnostics is not null, adds any warnings/errors to it.
+        /// Returns null if not successful.
+        /// </summary>
+        public static Func<R, T1> GetFunc<R, T1>(string body, List<Diagnostic> diagnostics, string arg1Name)
+        {
+            Assembly asm = CompileToAssembly(
+                GetScriptCode(body, typeof(R), new[] {
+                    (typeof(T1), arg1Name)
+                }),
+                diagnostics
+            );
+            if (asm is null)
+                return null;
+            return (Func<R, T1>)Delegate.CreateDelegate(
+                typeof(Func<R, T1>),
+                asm.GetType("Script").GetMethod("Eval")
+            );
+        }
+
+        /// <summary>
+        /// Produces a Func<T> from the body of a C# method. 
+        /// This C# method body must return a value of type T.
+        /// If diagnostics is not null, adds any warnings/errors to it.
+        /// Returns null if not successful.
+        /// </summary>
+        public static Func<R, T1, T2> GetFunc<R, T1, T2>(
+            string body,
+            List<Diagnostic> diagnostics,
+            string arg1Name,
+            string arg2Name
+        )
+        {
+            Assembly asm = CompileToAssembly(
+                GetScriptCode(body, typeof(R), new[] {
+                    (typeof(T1), arg1Name),
+                    (typeof(T1), arg2Name)
+                }),
+                diagnostics
+            );
+            if (asm is null)
+                return null;
+            return (Func<R, T1, T2>)Delegate.CreateDelegate(
+                typeof(Func<R, T1, T2>),
                 asm.GetType("Script").GetMethod("Eval")
             );
         }
