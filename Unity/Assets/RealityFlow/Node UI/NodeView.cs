f0c48b1c1e3bbb0688fbb4db9a5d831ed92cf265
PROMETHIA <electriccobras@gmail.com>
2024-05-22 18:59:53 -0400

    Add node prefab prototype

---
+++ Unity/Assets/RealityFlow/Node UI/NodeView.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class NodeView : MonoBehaviour
+{
+    public Node Node { get; set;}
+
+    TextMeshPro title;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+
+    }
+}
