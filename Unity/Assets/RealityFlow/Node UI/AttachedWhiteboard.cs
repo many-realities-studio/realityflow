40b5429fc1ec707f6ae71fe0a7c0212fe93b8c7d
Mathew-Reilly <mathewreilly2735@outlook.com>
2024-06-24 00:02:31 -0400

    whiteboard event

--- Unity/Assets/RealityFlow/Node UI/AttachedWhiteboard.cs
+++ Unity/Assets/RealityFlow/Node UI/AttachedWhiteboard.cs
@@ -57,7 +57,7 @@ namespace RealityFlow.NodeUI
                 whiteboard.SetActive(false);
             }
 
-            GetComponent<ObjectManipulator>().selectExitedgit .AddListener(_ => 
+            GetComponent<ObjectManipulator>().selectExited.AddListener(_ => 
             {
                 if (PlayManager == null || !PlayManager.playMode || RealityFlowAPI.Instance.SpawnedObjects[gameObject]!=null)
                     ShowWhiteboard(gameObject);
