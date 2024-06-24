c4e974a29bc49c3149e1de2d917acf922d4bf6a9
Jon0leary <jomosoftware@gmail.com>
2024-06-23 23:32:42 -0400

    Leave Room No Longer Mapped to B on the Right Hand

--- Unity/Assets/RealityFlow/Scripts/EnableNearMenuToolbox.cs
+++ Unity/Assets/RealityFlow/Scripts/EnableNearMenuToolbox.cs
@@ -1,20 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class EnableNearMenuToolbox : MonoBehaviour
 {
     // Reference to the Near Menu Toolbox GameObject
     public GameObject nearMenuToolbox;
 
+    // Define an event that other scripts can subscribe to
+    public event UnityAction EnableNearMenuToolboxAction;
+
     // Method to toggle the visibility of the Near Menu Toolbox
     public void ToggleNearMenuToolbox()
     {
+        Debug.Log(" === TOGGLE NEAR MENU TOOLBOX ===");
         if (nearMenuToolbox != null)
         {
             bool isActive = nearMenuToolbox.activeSelf;
             nearMenuToolbox.SetActive(!isActive);
             Debug.Log("Near Menu Toolbox " + (!isActive ? "enabled" : "disabled") + ".");
+
+            // Invoke the event to notify subscribers
+            EnableNearMenuToolboxAction?.Invoke();
         }
         else
         {
