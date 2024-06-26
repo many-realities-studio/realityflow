f396aeed8095e87dd77a59e19ac19d254fdb566c
PROMETHIA <electriccobras@gmail.com>
2024-06-26 15:41:11 -0400

    fix vector3 + quaterion serialization

--- Unity/Assets/RealityFlow/Scripts/API/RealityFlowAPI.cs
+++ Unity/Assets/RealityFlow/Scripts/API/RealityFlowAPI.cs
@@ -1081,13 +1081,13 @@ public class RealityFlowAPI : MonoBehaviour, INetworkSpawnable
             if (reader.TokenType == JsonToken.Null)
             {
                 if (objectType.IsNullable() == false)
-                    throw new JsonSerializationException("Cannot convert null value to KeyValuePair.");
+                    throw new JsonSerializationException("Cannot convert null value to Vector3.");
 
                 return null;
             }
 
             if (reader.TokenType != JsonToken.StartObject)
-                throw new JsonSerializationException("A Vector3 must be deserialized as an object");
+                throw new JsonSerializationException("A Vector3 must be deserialized from an object");
 
             reader.Read();
 
@@ -1099,12 +1099,16 @@ public class RealityFlowAPI : MonoBehaviour, INetworkSpawnable
                 if (reader.TokenType != JsonToken.Float)
                     throw new JsonSerializationException("Vector3 properties must be floats");
                 _ = property switch {
-                    "x" => value.x = (float)reader.Value,
-                    "y" => value.y = (float)reader.Value,
-                    "z" => value.z = (float)reader.Value,
+                    "x" => value.x = (float)(double)reader.Value,
+                    "y" => value.y = (float)(double)reader.Value,
+                    "z" => value.z = (float)(double)reader.Value,
                     _ => throw new JsonSerializationException($"Unknown Vector3 property {property} encountered"),
                 };
+                reader.Read();
             }
+            
+            if (reader.TokenType != JsonToken.EndObject)
+                throw new JsonSerializationException("A Vector3 must be be ended with EndObject");
 
             return value;
         }
@@ -1124,10 +1128,73 @@ public class RealityFlowAPI : MonoBehaviour, INetworkSpawnable
         }
     }
 
+    class QuaternionConverter : JsonConverter
+    {
+        public override bool CanConvert(Type objectType)
+            => objectType == typeof(Quaternion);
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            if (reader.TokenType == JsonToken.Null)
+            {
+                if (objectType.IsNullable() == false)
+                    throw new JsonSerializationException("Cannot convert null value to Quaternion.");
+
+                return null;
+            }
+
+            if (reader.TokenType != JsonToken.StartObject)
+                throw new JsonSerializationException("A Quaternion must be deserialized from an object");
+
+            reader.Read();
+
+            Quaternion value = Quaternion.identity;
+            while (reader.TokenType == JsonToken.PropertyName)
+            {
+                string property = (string)reader.Value;
+                reader.Read();
+                if (reader.TokenType != JsonToken.Float)
+                    throw new JsonSerializationException("Quaternion properties must be floats");
+                _ = property switch
+                {
+                    "x" => value.x = (float)(double)reader.Value,
+                    "y" => value.y = (float)(double)reader.Value,
+                    "z" => value.z = (float)(double)reader.Value,
+                    "w" => value.w = (float)(double)reader.Value,
+                    _ => throw new JsonSerializationException($"Unknown Quaternion property {property} encountered"),
+                };
+                reader.Read();
+            }
+
+            if (reader.TokenType != JsonToken.EndObject)
+                throw new JsonSerializationException("A Quaternion must be be ended with EndObject");
+
+            return value;
+        }
+
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            Quaternion quat = (Quaternion)value;
+
+            writer.WriteStartObject();
+            writer.WritePropertyName("x");
+            writer.WriteValue(quat.x);
+            writer.WritePropertyName("y");
+            writer.WriteValue(quat.y);
+            writer.WritePropertyName("z");
+            writer.WriteValue(quat.z);
+            writer.WritePropertyName("w");
+            writer.WriteValue(quat.w);
+            writer.WriteEndObject();
+        }
+    }
+
     public void LogActionToServer(string action, object data)
     {
-        JsonSerializer ser = new();
+        JsonSerializer ser = JsonSerializer.CreateDefault();
         ser.Converters.Add(new Vector3Converter());
+        ser.Converters.Add(new QuaternionConverter());
+
         var createObject = new GraphQLRequest
         {
             Query = @"
