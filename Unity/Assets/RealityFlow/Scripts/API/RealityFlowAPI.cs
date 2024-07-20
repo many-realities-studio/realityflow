b6df9e0793d759181cf85ca65fc0ff5d7228ad7e
Jordan Miller <123897858+JMiller007@users.noreply.github.com>
2024-07-20 15:15:33 -0400

    Reapply "Merge branch 'main' into JMiller007/EscapeRoomExample"
    This reverts commit efdfde1bfcd2029f359c46bd09dfaf385f428ac6.

--- Unity/Assets/RealityFlow/Scripts/API/RealityFlowAPI.cs
+++ Unity/Assets/RealityFlow/Scripts/API/RealityFlowAPI.cs
@@ -33,7 +33,7 @@ using TMPro;
 using RealityFlow.Collections;
 using RealityFlow.Scripting;
 using Newtonsoft.Json.Converters;
-//using UnityEditor.U2D;
+// using UnityEditor.U2D; REMOVED FOR COMPILATION
 
 
 #if UNITY_EDITOR
@@ -1305,8 +1305,7 @@ public class RealityFlowAPI : MonoBehaviour, INetworkSpawnable
 
                             // Update dictionary with the original prefab name
                             UpdateObjectToPrefabNameDictionary(returnedId, prefabName);
-
-
+                            spawnedObject.GetComponent<MyNetworkedObject>().UpdateRfObject(rfObject);
                             LogActionToServer("SpawnObject", new { rfObject });
 
                             // Update the name of the spawned object in the scene
@@ -1383,7 +1382,16 @@ public class RealityFlowAPI : MonoBehaviour, INetworkSpawnable
         return spawnedObject;
     }
     #endregion
-
+    public void RegisterPeerSpawnedObject(GameObject obj, RfObject rfObj)
+    {
+        if (spawnedObjectsById.ContainsKey(rfObj.id))
+        {
+            Debug.LogWarning("Object with ID " + rfObj.id + " already exists in the spawnedObjectsById dictionary.");
+            return;
+        }
+        spawnedObjects[obj] = rfObj;
+        spawnedObjectsById[rfObj.id] = obj;
+    }
     readonly HashSet<GameObject> nonPersistentObjects = new();
 
     public void InstantiateNonPersisted(GameObject obj, Vector3 position, Quaternion rotation)
@@ -1763,30 +1771,6 @@ public class RealityFlowAPI : MonoBehaviour, INetworkSpawnable
 
                     var serializableMesh = JsonUtility.FromJson<SerializableMeshInfo>(obj.meshJson);
                     // Deserialize the two dimensional array of integers from the json string and assign it to serializableMesh.faces
-
-                    // StringBuilder sb = new StringBuilder();
-                    // sb.Append("[");
-                    // for (int i = 0; i < smi.faces.Length; i++)
-                    // {
-                    //     sb.Append("[");
-                    //     for (int j = 0; j < smi.faces[i].Length; j++)
-                    //     {
-                    //         sb.Append(smi.faces[i][j]);
-                    //         if (j < smi.faces[i].Length - 1)
-                    //         {
-                    //             sb.Append(",");
-                    //         }
-                    //     }
-                    //     sb.Append("]");
-                    //     if (i < smi.faces.Length - 1)
-                    //     {
-                    //         sb.Append(",");
-                    //     }
-                    // }
-                    // sb.Append("]");
-
-                    // Reverse the above serialization for the faces property of the string contained in obj.meshJson and assign it to serializableMesh.faces
-                    //
                     obj.meshJson = obj.meshJson.Remove(obj.meshJson.Length - 1);
                     int start = obj.meshJson.LastIndexOf("\"faces\":") + 9;
                     int end = obj.meshJson.Length;
@@ -1798,6 +1782,7 @@ public class RealityFlowAPI : MonoBehaviour, INetworkSpawnable
                         // If the last character is a , remove it
                         faceArray[i] = faceArray[i].TrimEnd(',');
                         string[] face = faceArray[i].Split(',');
+
                         // Remove any "]" characters from the last element of the array
                         facesArray[i - 1] = new int[face.Length];
                         for (int j = 0; j < face.Length; j++)
--- Unity/Assets/RealityFlow/Scripts/CustomObjectManipulator.cs
+++ Unity/Assets/RealityFlow/Scripts/CustomObjectManipulator.cs
@@ -2,9 +2,13 @@ using UnityEngine;
 using Microsoft.MixedReality.Toolkit.SpatialManipulation;
 using RealityFlow; // Ensure RealityFlow namespace is correct
 using UnityEngine.XR.Interaction.Toolkit;
+using System.Collections;
 
 public class CustomObjectManipulator : ObjectManipulator
 {
+    private MyNetworkedObject networkedObject;
+    private Coroutine updateTransformCoroutine;
+
     protected override void OnSelectEntered(SelectEnterEventArgs args)
     {
 
@@ -13,42 +17,6 @@ public class CustomObjectManipulator : ObjectManipulator
         // Log the transformation when manipulation starts (object is grabbed)
         if (HostTransform != null)
         {
-            /*var rfObj = RealityFlowAPI.Instance.FindSpawnedObject(HostTransform.gameObject.name);
-            Debug.Log("Entered on select should log " + rfObj.name);
-            RealityFlowAPI.Instance.actionLogger.LogAction(
-                            nameof(RealityFlowAPI.UpdateObjectTransform),
-                            rfObj.name,
-                            HostTransform.position,
-                            HostTransform.rotation,
-                            HostTransform.localScale
-                        );*/
-        }
-        else
-            Debug.Log("HostTransform is null");
-
-        // Debug log to print the current location
-        Debug.Log("Current location of HostTransform: position: " + HostTransform.position +
-                  " rotation: " + HostTransform.rotation +
-                  " scale: " + HostTransform.localScale);
-    }
-
-    /*
-    protected override void OnSelectExited(SelectExitEventArgs args)
-    {
-        base.OnSelectExited(args);
-
-        // Log the transformation when manipulation ends (object is released)
-        if (HostTransform != null)
-        {
-            var rfObj = RealityFlowAPI.Instance.SpawnedObjects[HostTransform.gameObject];
-            RealityFlowAPI.Instance.actionLogger.LogAction(
-                nameof(RealityFlowAPI.UpdateObjectTransform),
-                rfObj.id,
-                HostTransform.position,
-                HostTransform.rotation,
-                HostTransform.localScale
-            );
         }
     }
-    */
 }
