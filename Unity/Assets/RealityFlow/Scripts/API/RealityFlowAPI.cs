80ddba362405a128a6f18a93e5b070155f07c2ba
Jon0leary <jomosoftware@gmail.com>
2024-07-21 22:27:20 -0400

    QuickFix

--- Unity/Assets/RealityFlow/Scripts/API/RealityFlowAPI.cs
+++ Unity/Assets/RealityFlow/Scripts/API/RealityFlowAPI.cs
@@ -1051,9 +1051,6 @@ public class RealityFlowAPI : MonoBehaviour, INetworkSpawnable
 
                 Debug.Log("THE CURRENT OBJECT IS: " + spawnedPrefab);
 
-                // Update the object's RfObject component[?]
-                spawnedPrefab.GetComponent<NetworkedPrefab>().update(rfObject);
-
                 LogActionToServer("SpawnObject", new { rfObject });
 
                 // Update the name of the spawned object in the scene
